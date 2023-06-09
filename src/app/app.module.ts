867035dfb244f651aec85dfcab49fed06f8eecce
August Bright <augustbright@yandex.com>
2023-06-09 17:38:20 +0000

    Update 8 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GameModule } from './game/game.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, GameModule],
+  imports: [BrowserModule, GameModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WindowComponent } from './window/window.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [WindowComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule],
   exports: [WindowComponent],
 })
 export class GameModule {}
